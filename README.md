4d26bece302ef2cb4ec858a731b4fce653146038
soraly <lequangnam1617@gmail.com>
2021-08-31 22:08:25 +0700

    add circle center point of the object

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # measure-object
+![Demo Draw Center Point](https://res.cloudinary.com/soralymusic/image/upload/v1630422378/Python/Screen_Shot_2021-08-31_at_22.06.05_pzctec.png) 
--- measure-object.py
+++ measure-object.py
@@ -11,10 +11,17 @@ contours = detector.detect_objects(img)
 # Draw objects boundaries
 
 for cnt in contours:
-    # Vẽ Khung bao quanh object (polylines)
+    # Vẽ Khung(contours) bao quanh object (polylines)
     cv2.polylines(img, [cnt], True, (255, 0, 0), 2)
 
     # Get rectangle when draw polylines
+    (x, y), (w, h), angle = cv2.minAreaRect(cnt)
+    # Draw point center of object with color orange and size 2 pixels
+    cv2.circle(img, (int(x), int(y)), 2, (0, 0, 255), -1)
+
+    print(x, y)  # 297.5 and 171.5 ? x and y ? => (x,y) là center point of the object -> Tọa độ trung tâm của object
+    print(w, h)   # 311.0 and 145.0
+    print(angle)   # 90(độ)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
