ada1d5567e250add04addb86b7e8eb6ef6e92dda
soraly <lequangnam1617@gmail.com>
2021-08-31 22:49:21 +0700

    draw width and height but sometime went wrong :v

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Learn Python With OpenCV2
 ### Draw Center Point Of the Object
 ![Demo Draw Center Point](https://res.cloudinary.com/soralymusic/image/upload/v1630422378/Python/Screen_Shot_2021-08-31_at_22.06.05_pzctec.png) 
+
+### Success Print Height and Width But Wait Minutes :v WTF Some Wrongs.
+![Demo Success](https://res.cloudinary.com/soralymusic/image/upload/v1630424852/Python/Screen_Shot_2021-08-31_at_22.47.20_uxzol2.png)
--- measure-object.py
+++ measure-object.py
@@ -15,18 +15,18 @@ for cnt in contours:
     # Get rectangle when draw polylines
     rectangle = cv2.minAreaRect(cnt)
     (x, y), (w, h), angle = rectangle
-
-    # Draw point center of object with color orange and size 2 pixels
-    cv2.circle(img, (int(x), int(y)), 2, (0, 0, 255), -1)
-
-    #
+    # DIsplay box rectangle
     box = cv2.boxPoints(rectangle)
     # Làm tròn số
     box = np.int0(box)
 
+    # Draw point center of object with color orange and size 2 pixels
+    cv2.circle(img, (int(x), int(y)), 2, (0, 0, 255), -1)
     # Vẽ Khung(contours) bao quanh object (polylines)
-    cv2.polylines(img, [cnt], True, (255, 0, 0), 2)
-
+    cv2.polylines(img, [box], True, (255, 0, 0), 2)
+#     Display width and height on the picture
+    cv2.putText(img, "Width {}".format(round(w, 1)), (int(x + 10), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 0.75, (25, 144, 255), 1)
+    cv2.putText(img, "Height {}".format(round(h, 1)), (int(x + 10), int(y)), cv2.FONT_HERSHEY_PLAIN, 0.75, (25, 144, 255), 1)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
